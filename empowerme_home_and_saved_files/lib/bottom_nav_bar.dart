2cc7a6a34511f8295ab85ee8dad92a3af4b9a12b
diw02 <dinukaw11@gmail.com>
2024-02-05 21:57:40 +0530

    home, saved and nav bar v1

---
+++ empowerme_home_and_saved_files/lib/bottom_nav_bar.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key, required this.onTap});
+
+  final Function onTap; // Define the onTap function as a property
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int selectedIndex = 0; // Define selectedIndex for tracking the selected item
+  bool _isPlaying = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            blurRadius: 10, // Adjust blur radius for desired softness
+            offset: const Offset(0, -2), // Apply shadow above the bar
+          ),
+        ],
+      ),
+      child: BottomNavigationBar(
+        elevation: 10, // Add elevation for visual separation
+        currentIndex: selectedIndex,
+        onTap: (index) {
+          if (index == 1) { // Check if tapped index is 1 (Detect)
+            setState(() {
+              _isPlaying = !_isPlaying; // Toggle play/pause state
+              widget.onTap(index); // Call provided function
+            });
+          } else {
+            setState(() {
+              selectedIndex = index;
+              widget.onTap(index);
+            });
+          }
+        },
+        items: [
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined, size: 40),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Container(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              padding: const EdgeInsets.only(top: 5, bottom: 5, left: 30, right: 30),
+                child: _isPlaying ? const Icon(Icons.pause_rounded, size: 40) : const Icon(Icons.play_arrow_rounded, size: 40),
+            ),
+            label: 'Detect',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline_rounded, size: 40),
+            label: 'Profile',
+          ),
+        ],
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+      ),
+    );
+
+  }
+}
---
+++ empowerme_home_and_saved_files/lib/home_page.dart
@@ -0,0 +1,152 @@
+import 'package:dinuka_empower_me/bottom_nav_bar.dart';
+import 'package:dinuka_empower_me/saved_files.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  List <String> menuImages = ['assets/images-home/record.png','assets/images-home/watch.png','assets/images-home/savedfiles.png','assets/images-home/upload.png','assets/images-home/settings.png','assets/images-home/cloud.png'];
+  List <String> mainMenu = ['Record','Watch','Saved Files', 'Upload','Settings','Cloud'];
+
+  //keeps track of current page to display
+  int selectedIndex = 0;
+  void navigateBottomBar(int index){
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  emergencyCall(){} //method for the emergency call button in the app bar
+
+  void menuPressed(int index) {
+    switch (index){
+      case 1:
+        //code for record page
+      case 2:
+        //code for watch page
+      case 3:
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => SavedFilesPage()),
+        );
+        break;
+      case 4:
+        //code for upload page
+      case 5:
+        //code for settings page
+      case 6:
+        //code for cloud page
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: const Text("Home"),
+        titleTextStyle: const TextStyle(
+          fontSize: 40,
+          fontWeight: FontWeight.w800,
+          color: Color.fromRGBO(0, 124, 207, 1.0),
+        ),
+        backgroundColor: const Color.fromRGBO(0, 153, 255, 1.0),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.call,
+              size: 30,
+              color: Colors.white,
+            ),
+            onPressed: () => emergencyCall(), // define onPressed logic here
+          ),
+        ],
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.only(top: 70, left: 50, right: 50, bottom: 50),
+        child: Center(
+          child: GridView.builder(
+            itemCount: mainMenu.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 30,
+              crossAxisSpacing: 30,
+            ),
+            itemBuilder: (context, index) {
+              final word = mainMenu[index];
+              final image = menuImages[index];
+
+              return Container(
+                decoration: BoxDecoration(
+                  color: Colors.white, // Button's background color
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.5),
+                      blurRadius: 10, // Adjust blur radius as needed
+                      offset: const Offset(2, 5), // Adjust offset for shadow position
+                    ),
+                  ],
+                ),
+                child: ElevatedButton(
+                  onPressed: () {
+                    // if index is pressed, goes to switch-case.
+                    menuPressed(index);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.transparent, // Make background transparent
+                    elevation: 0, // Remove default elevation
+                    padding: const EdgeInsets.all(10), // Button's internal padding
+                  ),
+                  child: Stack(
+                    children: [
+                      // Position text at top left
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          word,
+                          style: const TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                      // Position image within the button container
+                      Positioned(
+                        top: 50, // Adjust vertical position as needed
+                        left: 50, // Adjust horizontal position as needed
+                        child: Image.asset(
+                          image,
+                          width: 60,
+                          height: 60,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+
+
+
+
+
+      bottomNavigationBar: BottomNavBar(
+        onTap: (index) => navigateBottomBar(index),
+      ),
+
+    );
+  }
+
+}
