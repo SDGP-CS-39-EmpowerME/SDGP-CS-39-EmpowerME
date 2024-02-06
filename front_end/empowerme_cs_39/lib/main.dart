30131e600415fbb957fa1dd786c4fd62eece6be6
diw02 <dinukaw11@gmail.com>
2024-02-06 13:16:00 +0530

    connected home,nav,savedfiles with watch page. nav bar not working properly.

--- front_end/empowerme_cs_39/lib/main.dart
+++ front_end/empowerme_cs_39/lib/main.dart
@@ -1,18 +1,23 @@
 
 // ignore_for_file: unnecessary_new
 import 'package:flutter/material.dart';
+import 'package:empowerme_cs_39/home_page.dart';
+import 'package:empowerme_cs_39/saved_files.dart';
 import 'package:empowerme_cs_39/smartwatch_details_page.dart';
 
 void main() {
 runApp(MaterialApp(
+  title: 'EmpowerMe App',
   debugShowCheckedModeBanner: false,
   theme: ThemeData(
       colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
       useMaterial3:true,
       ),
-  home: new SmartwatchDetails(), //specify the screen the app starts on.
+  home: HomePage(), //specify the screen the app starts on.
   routes: <String, WidgetBuilder>{
     '/smartwatch_details_page': (BuildContext context) => SmartwatchDetails(),
+    '/homepage' :(BuildContext context) =>  HomePage(),
+    '/savedfilespage' :(BuildContext context) => SavedFilesPage(),
   },
 ));
 }
